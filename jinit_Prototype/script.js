4fc8c40d4e3dbf02fce64c877441bb2b697671cf
jinit2000 <jinitpatel4112000@gmail.com>
2024-12-05 16:13:14 -0500

    Initial commit

---
+++ jinit_Prototype/script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log("Time Café website is operational.");
